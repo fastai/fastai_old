2466425b0a347fa419a411ef370ce0044b843d35
Jeremy Howard <j@howard.fm>
2018-08-02 07:14:07 -0700

    fix notebook2script.py

--- dev_nb/notebook2script.py
+++ dev_nb/notebook2script.py
@@ -1,17 +1,18 @@
-import json, fire
+import json, fire, re
 
 def is_export(cell):
     if cell['cell_type'] != 'code': return False
-    if len(cell['source']) == 0 or len(cell['source'][0]) < 7: return False
-    return re.match('\s*#export\s*', cell['source'][0], re.IGNORECASE) is not None
+    src = cell['source']
+    if len(src) == 0 or len(src[0]) < 7: return False
+    #import pdb; pdb.set_trace()
+    return re.match('^\s*#export\s*$', src[0], re.IGNORECASE) is not None
 
 def notebook2script(fname):
     main_dic = json.load(open(fname,'r'))
     cells = main_dic['cells']
     code_cells = [c for c in cells if is_export(c)]
     module = ''
-    for cell in code_cells:
-        module += ''.join(cell['source'][1:]) + '\n\n'
+    for cell in code_cells: module += ''.join(cell['source'][1:]) + '\n\n'
     number = file_name.split('_')[0]
     with open(f'nb_{number}.py','w') as f: f.write(module[:-2])
 
