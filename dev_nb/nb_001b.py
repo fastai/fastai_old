542fc005a319099b53ce8ad69a08540d958dae7c
Jeremy Howard <j@howard.fm>
2018-08-03 17:08:14 -0700

    remove dl transforms

--- dev_nb/nb_001b.py
+++ dev_nb/nb_001b.py
@@ -2,10 +2,11 @@ import pickle, gzip, torch, math, numpy as np, torch.nn.functional as F
 from pathlib import Path
 from IPython.core.debugger import set_trace
 from torch import nn, optim
-from torch.utils.data import TensorDataset, DataLoader
-from collections import Iterable
-from functools import reduce,partial
-from tqdm import tqdm, tqdm_notebook, trange, tnrange
+
+from torch.utils.data import TensorDataset, DataLoader, Dataset
+from dataclasses import dataclass
+from typing import Callable
+from functools import partial
 
 def loss_batch(model, xb, yb, loss_fn, opt=None):
     loss = loss_fn(model(xb), yb)
@@ -14,17 +15,13 @@ def loss_batch(model, xb, yb, loss_fn, opt=None):
         loss.backward()
         opt.step()
         opt.zero_grad()
-
+        
     return loss.item(), len(xb)
 
-
 def fit(epochs, model, loss_fn, opt, train_dl, valid_dl):
-    for epoch in tnrange(epochs):
+    for epoch in range(epochs):
         model.train()
-        it = tqdm_notebook(train_dl, leave=False)
-        for xb,yb in it:
-            loss,_ = loss_batch(model, xb, yb, loss_fn, opt)
-            it.set_postfix_str(loss)
+        for xb,yb in train_dl: loss_batch(model, xb, yb, loss_fn, opt)
 
         model.eval()
         with torch.no_grad():
@@ -34,50 +31,28 @@ def fit(epochs, model, loss_fn, opt, train_dl, valid_dl):
 
         print(epoch, val_loss)
 
-
 class Lambda(nn.Module):
     def __init__(self, func):
         super().__init__()
         self.func=func
-
+        
     def forward(self, x): return self.func(x)
 
 def ResizeBatch(*size): return Lambda(lambda x: x.view((-1,)+size))
 def Flatten(): return Lambda(lambda x: x.view((x.size(0), -1)))
 def PoolFlatten(): return nn.Sequential(nn.AdaptiveAvgPool2d(1), Flatten())
 
-
-def is_listy(x): return isinstance(x, (list,tuple))
-
-def listify(p=None, q=None):
-    if p is None: p=[]
-    elif not is_listy(p): p=[p]
-    n = q if type(q)==int else 1 if q is None else len(q)
-    if len(p)==1: p = p * n
-    return p
-
-def compose(funcs):
-    def _inner(p, q=None):
-        if q is None:
-            for f in listify(funcs): p = f(p)
-            return p
-        else:
-            for f in listify(funcs): p,q = f(p,q)
-            return p,q
-    return _inner
-
-    #funcs = reversed(listify(funcs))
-    #return reduce(lambda f, g: lambda x: f(g(x)), funcs, lambda x: x)
-
-class IterPipe():
-    def __init__(self, iterator, funcs): self.iter,self.func = iterator,compose(funcs)
-    def __len__(self): return len(self.iter)
-    def __iter__(self): return map(self.func, self.iter)
-
-
-def get_dl(ds, bs, shuffle, tfms=None):
-    return IterPipe(DataLoader(ds, batch_size=bs, shuffle=shuffle), tfms)
-
+@dataclass
+class TfmDataset(Dataset):
+    ds: Dataset
+    tfm: Callable = None
+        
+    def __len__(self): return len(self.ds)
+    
+    def __getitem__(self,idx):
+        x,y = self.ds[idx]
+        if self.tfm is not None: x = self.tfm(x)
+        return x,y
 
 def conv2_relu(nif, nof, ks, stride):
     return nn.Sequential(nn.Conv2d(nif, nof, ks, stride, padding=ks//2), nn.ReLU())
@@ -88,18 +63,55 @@ def simple_cnn(actns, kernel_szs, strides):
     layers.append(PoolFlatten())
     return nn.Sequential(*layers)
 
+from tqdm import tqdm, tqdm_notebook, trange, tnrange
+from ipykernel.kernelapp import IPKernelApp
+
+def in_notebook(): return IPKernelApp.initialized()
 
 def to_device(device, b): return [o.to(device) for o in b]
-
 default_device = torch.device('cuda')
 
+if in_notebook():
+    tqdm = tqdm_notebook
+    trange = tnrange
+
+@dataclass
+class DeviceDataLoader():
+    dl: DataLoader
+    device: torch.device
+    progress_func: Callable = tqdm_notebook
+        
+    def __len__(self): return len(self.dl)
+    def __iter__(self):
+        self.gen = (to_device(self.device,o) for o in self.dl)
+        if self.progress_func is not None:
+            self.gen = self.progress_func(self.gen, total=len(self.dl), leave=False)
+        return iter(self.gen)
+
+    @classmethod
+    def create(cls, *args, device=default_device, **kwargs):
+        return cls(DataLoader(*args, **kwargs), device=device)
+
+def fit(epochs, model, loss_fn, opt, train_dl, valid_dl):
+    for epoch in tnrange(epochs):
+        model.train()
+        for xb,yb in train_dl:
+            loss,_ = loss_batch(model, xb, yb, loss_fn, opt)
+            if train_dl.progress_func is not None: train_dl.gen.set_postfix_str(loss)
+
+        model.eval()
+        with torch.no_grad():
+            losses,nums = zip(*[loss_batch(model, xb, yb, loss_fn)
+                                for xb,yb in valid_dl])
+        val_loss = np.sum(np.multiply(losses,nums)) / np.sum(nums)
+
+        print(epoch, val_loss)
 
 class DataBunch():
-    def __init__(self, train_ds, valid_ds, bs=64, device=None, train_tfms=None, valid_tfms=None):
+    def __init__(self, train_ds, valid_ds, bs=64, device=None, train_tfm=None, valid_tfm=None):
         self.device = default_device if device is None else device
-        dev_tfm = [partial(to_device, self.device)]
-        self.train_dl = get_dl(train_ds, bs,   shuffle=True,  tfms=dev_tfm + listify(train_tfms))
-        self.valid_dl = get_dl(valid_ds, bs*2, shuffle=False, tfms=dev_tfm + listify(valid_tfms))
+        self.train_dl = DeviceDataLoader.create(TfmDataset(train_ds,train_tfm), bs, shuffle=True)
+        self.valid_dl = DeviceDataLoader.create(TfmDataset(valid_ds, valid_tfm), bs*2, shuffle=False)
 
 class Learner():
     def __init__(self, data, model):
@@ -109,4 +121,3 @@ class Learner():
         opt = opt_fn(self.model.parameters(), lr=lr)
         loss_fn = F.cross_entropy
         fit(epochs, self.model, loss_fn, opt, self.data.train_dl, self.data.valid_dl)
-
