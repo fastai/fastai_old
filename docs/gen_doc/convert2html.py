2abbc914e6a1d93048ac8b7370c93f9f94a9a2ea
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-08-13 04:39:14 +0200

    First sample doc

--- docs/gen_doc/convert2html.py
+++ docs/gen_doc/convert2html.py
@@ -6,6 +6,9 @@ from pathlib import Path
 import fire
 
 class HandleLinksPreprocessor(Preprocessor):
+    """
+    A preprocesser that replaces all the .ipynb by .html in links.
+    """
     def preprocess_cell(self, cell, resources, index):
         if 'source' in cell and cell.cell_type == "markdown":
             cell.source = re.sub(r"\((.*)\.ipynb(.*)\)",r"(\1.html\2)",cell.source)
@@ -19,21 +22,28 @@ exporter.template_path.append(jupyter_contrib_nbextensions.__path__[0] + '\\temp
 #Preprocesser that converts the .ipynb links in .html
 exporter.register_preprocessor(HandleLinksPreprocessor, enabled=True)
 
+__all__ = ['convert_nb', 'convert_all']
+
 def read_nb(fname):
     with open(fname,'r') as f:
         return nbformat.reads(f.read(), as_version=4)
 
 def convert_nb(fname):
+    """
+    Converts a given notebook in an html page.
+    """
     nb = read_nb(fname)
     new_name = re.sub(r"(.*)\.ipynb",r"\1.html",str(fname))
     with open(new_name,'w') as f:
         f.write(exporter.from_notebook_node(nb)[0])
 
-PATH = Path('Results/')
-convert_nb(PATH/'fastai_v1.nb_001b.ipynb')
-
-def convert_all(path):
+def convert_all(folder):
+    """
+    Converts all the notebooks in a given folder in a html pages.
+    """
+    path = Path(folder)
     nb_files = path.glob('*.ipynb')
+    print(nb_files)
     for file in nb_files: convert_nb(file)
 
 if __name__ == 'main': fire.Fire(convert_all)
