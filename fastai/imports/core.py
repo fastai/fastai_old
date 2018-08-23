a33da89b3d556ca51aeddf6a5ef2d1efd9ec2d61
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-08-23 14:10:52 +0200

    Minor fixes

--- fastai/imports/core.py
+++ fastai/imports/core.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from dataclasses import dataclass
 from typing import Any, Collection, Callable, Union, Iterator, Tuple, NewType, List, Sequence, Dict
 from functools import partial, reduce
-from fast_progress.fast_progress import master_bar, progress_bar
+from fast_progress import master_bar, progress_bar
 from collections import defaultdict, abc, namedtuple, Iterable
