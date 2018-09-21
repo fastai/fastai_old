1e9fbd1c2f0d09900a37e2244ae8e69b7390da77
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-09-21 15:46:38 -0400

    Fix export cell

--- dev_nb/nb_007b.py
+++ dev_nb/nb_007b.py
@@ -6,6 +6,25 @@
 
 from nb_007a import *
 
+
+Weights = Dict[str,Tensor]
+
+def convert_weights(wgts:Weights, stoi_wgts:Dict[str,int], itos_new:Collection[str]) -> Weights:
+    "Converts the model weights to go with a new vocabulary."
+    dec_bias, enc_wgts = wgts['1.decoder.bias'], wgts['0.encoder.weight']
+    bias_m, wgts_m = dec_bias.mean(0), enc_wgts.mean(0)
+    new_w = enc_wgts.new_zeros((len(itos_new),enc_wgts.size(1))).zero_()
+    new_b = dec_bias.new_zeros((len(itos_new),)).zero_()
+    for i,w in enumerate(itos_new):
+        r = stoi_wgts[w] if w in stoi_wgts else -1
+        new_w[i] = enc_wgts[r] if r>=0 else wgts_m
+        new_b[i] = dec_bias[r] if r>=0 else bias_m
+    wgts['0.encoder.weight'] = new_w
+    wgts['0.encoder_dp.emb.weight'] = new_w.clone()
+    wgts['1.decoder.weight'] = new_w.clone()
+    wgts['1.decoder.bias'] = new_b
+    return wgts
+
 def lm_split(model:Model) -> List[Model]:
     "Splits a RNN model in groups for differential learning rates."
     groups = [nn.Sequential(rnn, dp) for rnn, dp in zip(model[0].rnns, model[0].hidden_dps)]
