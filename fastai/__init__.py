80504d8c51ce3cbfc80a459baad7887b90df7fce
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-09-24 13:24:46 -0400

    vision doc

--- fastai/__init__.py
+++ fastai/__init__.py
@@ -2,13 +2,13 @@ from .basic_train import *
 from .callback import *
 from .callbacks import *
 from .colab import *
-from .conv_learner import *
 from .core import *
 from .data import *
 from .layers import *
 from .metrics import *
-from .rnn_learner import *
+from .text_learner import *
 from .torch_core import *
 from .train import *
 from .tta import *
+from .vision_learner import *
 
