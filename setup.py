9dea2d09f9eb9c1a4326e2f6f0120c1860b93a80
Stas Bekman <stas@stason.org>
2018-09-24 11:44:03 -0700

    include all modules under fastai/

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     include_package_data=True,
     keywords='fastai',
     name='fastai',
-    packages=find_packages(include=['fastai']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
@@ -43,4 +43,3 @@ setup(
     version='1.0.0.beta1',
     zip_safe=False,
 )
-
