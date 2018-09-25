8dbc54429af4801a5b2e6a0cd86bb26d8f6a4b9e
Stas Bekman <stas@stason.org>
2018-09-24 22:13:25 -0700

    experiment with test parametrizing

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,15 +5,18 @@ from fastai import core
 def test_cpus():
     assert core.num_cpus() >= 1
 
-def test_listify():
-    assert core.listify(5,1)        == [5]
-    assert core.listify(5, [1,1])   == [5, 5]
-    assert core.listify([5],1)      == [5]
-    assert core.listify([5], [1,1]) == [5, 5]
-    assert core.listify("ab", "cd")           == ["a", "b"]
-    assert core.listify("ab", ["cd", "ef"])   == ["a", "b"]
-    assert core.listify(["ab"], "cd")         == ["ab", "ab"]
-    assert core.listify(["ab"], ["cd", "ef"]) == ["ab", "ab"]
+@pytest.mark.parametrize("p, q, expected", [
+    (5  , 1    , [5]),
+    (5  , [1,1], [5, 5]),
+    ([5], 1    , [5]),
+    ([5], [1,1], [5, 5]),
+    ("ab"  , "cd"        , ["a", "b"]),
+    ("ab"  , ["cd", "ef"], ["a", "b"]),
+    (["ab"], "cd"        , ["ab", "ab"]),
+    (["ab"], ["cd", "ef"], ["ab", "ab"]),
+])
+def test_listify(p, q, expected):
+    assert core.listify(p, q) == expected
 
 def test_ifnone():
     assert core.ifnone(None, 5) == 5
@@ -23,6 +26,7 @@ def test_uniqueify():
     assert core.uniqueify([1,1,3,3,5]) == [1,3,5]
     assert core.uniqueify([1,3,5])     == [1,3,5]
     assert core.uniqueify([1,1,1,3,5]) == [1,3,5]
+
 
 def test_listy():
     assert core.is_listy([1,1,3,3,5]) == True
